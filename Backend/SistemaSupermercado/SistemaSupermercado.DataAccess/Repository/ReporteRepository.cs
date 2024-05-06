34e2c72501d27ef2f8f18c1ed06faf57ff624e04
Pavelponce <136554909+PavelPonce@users.noreply.github.com>
2024-05-06 16:41:34 -0600

    Update ReporteRepository.cs

--- Backend/SistemaSupermercado/SistemaSupermercado.DataAccess/Repository/ReporteRepository.cs
+++ Backend/SistemaSupermercado/SistemaSupermercado.DataAccess/Repository/ReporteRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SistemaSupermercado.Entities.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace SistemaSupermercado.DataAccess.Repository
 {
     public class ReporteRepository
     {
+        public IEnumerable<tbProductos> reporteStock()
+        {
+            
+            return 
+        }
     }
 }
